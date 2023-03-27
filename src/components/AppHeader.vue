86b4113349b2831f481c57059c463837b43ca011
vittoriaromano <vittoria5005@gmail.com>
2023-03-27 19:01:17 +0400

    adding-font

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,5 +12,4 @@ export default{
 
 
 <style lang="scss" scoped>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './assets/scss/main.scss';
+// font
+import "@fontsource/montserrat";
 import App from './App.vue'
 
 createApp(App).mount('#app')
