d995e21a287522967a7c59fa02deaa2b732f0336
vittoriaromano <vittoria5005@gmail.com>
2023-03-27 18:32:09 +0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HelloApp from './components/HelloApp.vue';
+
+export default{
+  name: 'App',
+  components:{
+    HelloApp,
+  }
+}
+</script>
+
+<template>
+  <HelloApp></HelloApp>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/HelloApp.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    name: 'Hello',
+}
+</script>
+
+<template>
+    <h1>Hello <span>world</span></h1>
+</template>
+
+<style lang="scss" scoped>
+h1{
+    color: red;
+    span{
+        color: blue;
+    }
+}
+</style>
